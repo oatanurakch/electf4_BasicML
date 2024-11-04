3846d44f959bd369e5e044eb65b71b5400b9e0b8
oatanurakch <oatty6106164@gmail.com>
2024-11-04 23:38:16 +0700

    add example.ipynb

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = 'electf4_basicML',
-    version = '0.1.3',
+    version = '0.1.5',
     packages = find_packages(),
     install_requires = [
     ],
