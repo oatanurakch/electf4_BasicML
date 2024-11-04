5624e1f33678d28492b2f697ad43ac585bf0150b
oatanurakch <oatty6106164@gmail.com>
2024-11-04 23:44:45 +0700

    version 0.1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = 'electf4_basicML',
-    version = '0.1.5',
+    version = '0.1.7',
     packages = find_packages(),
     install_requires = [
     ],
